f184af182b2d9aa17a423a8f011034dadacf030b
NKimDL <deleon.kimberlynicole.9@gmail.com>
2025-09-10 11:55:29 +0800

    Add docker and composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'addd4fe13784e1762d60ddd11ea0f61b9199ef03',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'addd4fe13784e1762d60ddd11ea0f61b9199ef03',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
